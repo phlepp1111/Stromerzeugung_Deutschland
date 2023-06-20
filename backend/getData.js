58b31d6db4d72f9f03c2f4a614266af633c8fed9
phlepp1111 <phlpp.neumann@gmail.com>
2023-06-20 14:26:02 +0200

    chaining of the 2 getData-functions works now, only writing all data into a json for now

--- backend/getData.js
+++ backend/getData.js
@@ -1,38 +1,33 @@
+import { response } from "express";
 import fs from "fs";
 async function getTimestamps(filter, resolution) {
-    let timestamps = [];
-    fetch(
+    return fetch(
         `https://www.smard.de/app/chart_data/${filter}/DE/index_${resolution}.json`
     )
         .then((response) => response.json())
         .then((data) => {
-            data.timestamps.map((timestamp) => {
-                timestamps.push(timestamp);
-            });
-        })
-        .then(() => {
-            console.log(timestamps);
-            return timestamps;
+            console.log(data.timestamps);
+            return data.timestamps;
         });
 }
 
 async function getTimeline(filter, resolution) {
     getTimestamps(filter, resolution).then((timestamps) => {
         console.log(timestamps);
+        for (let timestamp of timestamps) {
+            fetch(
+                `https://www.smard.de/app/chart_data/${filter}/DE/${filter}_DE_${resolution}_${timestamp}.json`
+            )
+                .then((response) => response.json())
+                .then((data) => {
+                    fs.appendFileSync(
+                        "./data.json",
+                        JSON.stringify(data.series, null, 5)
+                    );
+                });
+        }
+        console.log("done.");
     });
-    // try {
-    //     const timestamps = await getTimestamps(filter, resolution);
-    //     for (let timestamp of timestamps) {
-    //         const response = await fetch(
-    //             `https://www.smard.de/app/chart_data/${filter}/DE/${filter}_DE_${resolution}_${timestamp}.json`
-    //         );
-    //         const data = await response.json();
-    //         fs.writeFileSync("./data.json", JSON.stringify(data, null, 5));
-    //     }
-    //     console.log("done.");
-    // } catch (error) {
-    //     console.log(error);
-    // }
 
     // let formattedData = data.series.map(([timestamp, value]) => {
     //     const formattedTimestamp = new Date(
