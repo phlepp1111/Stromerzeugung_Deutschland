0fe83196f96a703d0a5e13719b62a7dec5836303
phlepp1111 <phlpp.neumann@gmail.com>
2023-06-20 13:46:56 +0200

    trying to chain the 2 getData functions

--- backend/getData.js
+++ backend/getData.js
@@ -1,32 +1,45 @@
 import fs from "fs";
-function getTimestamps(filter, resolution) {
+async function getTimestamps(filter, resolution) {
+    let timestamps = [];
     fetch(
         `https://www.smard.de/app/chart_data/${filter}/DE/index_${resolution}.json`
     )
-        .then((answer) => answer.json())
+        .then((response) => response.json())
         .then((data) => {
-            console.log(data.timestamps.length);
-            // fs.writeFileSync(
-            //     "./dataTimestamps1225.json",
-            //     JSON.stringify(data, 5)
-            // );
-        });
-}
-getTimestamps(4387, "quarterhour");
-
-function getTimeline(filter, resolution) {
-    fetch(
-        `https://www.smard.de/app/chart_data/${filter}/DE/${filter}_DE_${resolution}_1686520800000.json`
-    )
-        .then((answer) => answer.json())
-        .then((data) => {
-            let formattedData = data.series.map(([timestamp, value]) => {
-                const formattedTimestamp = new Date(timestamp).toLocaleString(
-                    "de-DE"
-                );
-                return { [formattedTimestamp]: value };
+            data.timestamps.map((timestamp) => {
+                timestamps.push(timestamp);
             });
-            fs.writeFileSync("./data.json", JSON.stringify(formattedData, 5));
+        })
+        .then(() => {
+            console.log(timestamps);
+            return timestamps;
         });
 }
-// getTimeline(1223, "quarterhour");
+
+async function getTimeline(filter, resolution) {
+    getTimestamps(filter, resolution).then((timestamps) => {
+        console.log(timestamps);
+    });
+    // try {
+    //     const timestamps = await getTimestamps(filter, resolution);
+    //     for (let timestamp of timestamps) {
+    //         const response = await fetch(
+    //             `https://www.smard.de/app/chart_data/${filter}/DE/${filter}_DE_${resolution}_${timestamp}.json`
+    //         );
+    //         const data = await response.json();
+    //         fs.writeFileSync("./data.json", JSON.stringify(data, null, 5));
+    //     }
+    //     console.log("done.");
+    // } catch (error) {
+    //     console.log(error);
+    // }
+
+    // let formattedData = data.series.map(([timestamp, value]) => {
+    //     const formattedTimestamp = new Date(
+    //         timestamp
+    //     ).toLocaleString("de-DE");
+    //     return { [formattedTimestamp]: value };
+    // });
+}
+
+getTimeline(1223, "quarterhour");
--- backend/insertDB.js
+++ backend/insertDB.js
@@ -1,12 +1,10 @@
 import { db } from "./db.js";
 
-async function insertDB() {
+export default async function insertDB(table, timestamp, menge) {
     let conn;
     try {
         conn = await db.pool.getConnection();
-        const insertQuery =
-            "INSERT INTO StromDaten.BraunkohleErzeugung(Timestamp_Unix, Menge) VALUES (?,?)";
-        // console.log(db.pool);
+        const insertQuery = `INSERT INTO StromDaten.${table}(Timestamp_Unix, Menge) VALUES (${timestamp},${menge})`;
         const params = [1, 1];
         const result = await conn.query(insertQuery, params);
         console.log(`Eintrag (id=${result.insertId}) eingetragen.`);
@@ -16,21 +14,3 @@ async function insertDB() {
         if (conn) return conn.end();
     }
 }
-insertDB();
-// async function asyncFunction() {
-//     let conn;
-//     try {
-//         conn = await pool.getConnection();
-//         const rows = await conn.query("SELECT 1 as val");
-//         console.log(rows); //[ {val: 1}, meta: ... ]
-//         const res = await conn.query("INSERT INTO myTable value (?, ?)", [
-//             1,
-//             "mariadb",
-//         ]);
-//         console.log(res); // { affectedRows: 1, insertId: 1, warningStatus: 0 }
-//     } catch (err) {
-//         throw err;
-//     } finally {
-//         if (conn) return conn.end();
-//     }
-// }
