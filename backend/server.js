af8c753e6cfb5e70b7e3e433aaf6e5ae40e0e45e
phlepp1111 <phlpp.neumann@gmail.com>
2023-06-24 19:33:41 +0200

    server now serving vue.js boilerplate

--- backend/server.js
+++ backend/server.js
@@ -8,14 +8,14 @@ const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// app.use(express.static(path.join(__dirname, "..", "frontend")));
+app.use(express.static(path.join(__dirname, "..", "frontend", "dist")));
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
     console.log("received a get request");
-    res.sendFile(path.join(__dirname, "..", "frontend", "index.html"));
+    res.sendFile(path.join(__dirname, "..", "frontend", "dist", "index.html"));
 });
 
 app.listen(process.env.PORT || 3002, function () {
-    console.log("I'm listening.");
+    console.log("I'm listening on port 3002.");
 });
