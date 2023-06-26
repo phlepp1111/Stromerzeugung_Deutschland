522ab689a025b197bb2f4f1caeae9145b1c70e2b
phlepp1111 <phlpp.neumann@gmail.com>
2023-06-26 11:05:53 +0200

    modified the vueJs boilerplate

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,18 +1,17 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import StartPage from './components/StartPage.vue'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <StartPage msg="Here we go!" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/lastgraph">Last Graph</RouterLink>
       </nav>
     </div>
   </header>
---
+++ frontend/src/views/LastGraph.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="LastGraph">
+    <h1>Here should be the last Graph that we got from db</h1>
+  </div>
+</template>
